68cdefa3271e3cd4ca31ad74106baf2f654650f0
fh1798 <kjrychie@gmail.com>
2024-12-17 10:45:35 +0100

    modified:   mergedProject/include/footer.php 	modified:   mergedProject/include/nav.php 	modified:   mergedProject/include/navbar.php

--- mergedProject/include/footer.php
+++ mergedProject/include/footer.php
@@ -2,7 +2,7 @@
 <div class="container-fluid">
   <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary fw-bolder">Home</a></li>
+      <li class="nav-item"><a href="../index.php" class="nav-link px-2 text-body-secondary fw-bolder">Home</a></li>
       <li class="nav-item"><a href="zimmer.php" class="nav-link px-2 text-body-secondary fw-bolder">Zimmer buchen</a></li>
       <li class="nav-item"><a href="kontakt.php" class="nav-link px-2 text-body-secondary fw-bolder">Kontakt</a></li>
       <li class="nav-item"><a href="include/hilfeseite.php" class="nav-link px-2 text-body-secondary fw-bolder">FAQs</a></li>
