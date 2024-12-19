7a7124abd40d8f56ad1eae4cef8ddc9f9754a417
fh1798 <kjrychie@gmail.com>
2024-12-19 21:49:11 +0100

    modified:   mergedProject/include/fct_logout.php

--- mergedProject/include/fct_logout.php
+++ mergedProject/include/fct_logout.php
@@ -1,7 +1,6 @@
-<?php 
-// Header einbinden
-include '../include/header.php'; 
+<?php include '../include/header.php';  ?>
 
+<?php 
 // Session starten
 session_start();
 
@@ -9,10 +8,6 @@ session_start();
 if (isset($_GET['logout'])) {
     session_unset(); // Alle Session-Daten löschen
     session_destroy(); // Session zerstören
-    
-    // Weiterleitung zur Startseite nach dem Logout
-    header("Location: ../index.php"); // Leitet den Benutzer nach dem Logout zur Startseite weiter
-    exit(); // Beendet das Script hier, damit keine weitere Ausgabe erfolgt
 }
 
 ?>
@@ -22,13 +17,16 @@ if (isset($_GET['logout'])) {
     <div class="hero-content text-center text-white">
         <div class="container">
             <h2 class="display-4">Sie haben sich erfolgreich ausgeloggt</h2>
-            <p class="lead mb-4">Danke, dass Sie bei uns waren! Sie werden nun zur Startseite weitergeleitet.</p>
+            <p class="lead mb-4">Danke, dass Sie bei uns waren! Sie können nun zur Startseite zurück.</p>
             
             <!-- Optional: Button, um die Weiterleitung manuell zu triggern -->
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" class="btn btn-primary btn-lg fw-bolder px-4">
-                    <a href="../index.php" class="text-center text-white text-decoration-none">Zur Startseite</a>
-                </button>
+              <button type="button" class="btn btn-primary btn-lg fw-bolder px-4">
+                <a href="?logout=true" class="text-center text-white text-decoration-none">Logout</a>
+              </button>
+              <button type="button" class="btn btn-primary btn-lg fw-bolder px-4">
+                  <a href="../index.php" class="text-center text-white text-decoration-none">Zur Startseite</a>
+              </button>
             </div>  
         </div>
     </div>
