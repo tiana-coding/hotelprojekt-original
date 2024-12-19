76c295a63e366655208218a4c0b101dd0f461131
fh1798 <kjrychie@gmail.com>
2024-12-19 21:12:34 +0100

    modified:   mergedProject/include/fct_logout.php

--- mergedProject/include/fct_logout.php
+++ mergedProject/include/fct_logout.php
@@ -15,15 +15,9 @@ if (isset($_GET['logout'])) {
 <div class="container-fluid hero">
   
   <div class="hero-content text-center text-white">
-    <div class="container">
-
-        <h1 class="display-5 fw-bold">Blick and Glück</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4 display-7 fw-bold">Resort und SPA Hotel</p>
-        </div> 
+    <div class="container"> 
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" class="btn btn-primary btn-lg fw-bolder px-4"><a href="?logout=true" class="text-center text-white text-decoration-none">Logout</a></button>
-            
         </div>  
     </div>
   </div>
