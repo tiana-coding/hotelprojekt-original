7c3ae03de9685cc719f559ef61e9791c811fb0c5
tiana-coding <102430225+tiana-coding@users.noreply.github.com>
2024-12-20 04:33:34 +0100

    logout mit modal gebaut, noch nicht optimal aber erfüllt mal den zweck.

--- mergedProject/include/fct_logout.php
+++ mergedProject/include/fct_logout.php
@@ -1,36 +1,53 @@
-<?php include '../include/header.php';  ?>
+<?php
+include '../include/fct_session.php';
+include '../include/header.php';  ?>
 
 <?php 
 // Session starten
 session_start();
 
 // Logout-Mechanismus
-if (isset($_GET['logout'])) {
+//überprüfen, ob logout paramenter in der url vorhanden ist
+if (isset($_GET['logout'])&& $_GET['logout']=='true') {
     session_unset(); // Alle Session-Daten löschen
     session_destroy(); // Session zerstören
+    header("Location:../index.php?logout=success"); 
+    exit();
 }
 
 ?>
 
-<!-- Hero-Section für das Logout -->
-<div class="container-fluid hero">
-    <div class="hero-content text-center text-white">
-        <div class="container">
-            <h2 class="display-4">Sie haben sich erfolgreich ausgeloggt</h2>
-            <p class="lead mb-4">Danke, dass Sie bei uns waren! Sie können nun zur Startseite zurück.</p>
-            
-            <!-- Optional: Button, um die Weiterleitung manuell zu triggern -->
-            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <button type="button" class="btn btn-primary btn-lg fw-bolder px-4">
-                <a href="?logout=true" class="text-center text-white text-decoration-none">Logout</a>
-              </button>
-              <button type="button" class="btn btn-primary btn-lg fw-bolder px-4">
-                  <a href="../index.php" class="text-center text-white text-decoration-none">Zur Startseite</a>
-              </button>
-            </div>  
+<!-- Für das Logout mit modal -->
+<!-- trigger wenn es angeklickt wird, werden die optionen ausloggen oder abbrechen ausgelöst -->
+<div class="container text-center my-5 min-vh-100">
+    <div class="container border border-warning py-5">  
+        <p class="text-center">Wollen Sie wirklich ausloggen?</p>
+        <button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
+    </div>
+</div>
+
+<div class="modal fade" id="logoutModal"tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="logoutModalLabel">Logout</h5>
+            <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+
+        </div>
+        <div class="modal-body text-center">
+            <p>Sie werden jetzt ausgeloggt.</p>
+        </div>
+        <div class="modal-footer">
+        <!-- ausgeloggt durch checken ob logout paramenter den value=true hat  -->
+        <a href="?logout=true" class="btn btn-warning" role="alert">Bestätigen und Logout</a>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
         </div>
     </div>
-</div> 
+  </div>
+</div>
+
+
+
 
 <?php 
 // Footer einbinden
