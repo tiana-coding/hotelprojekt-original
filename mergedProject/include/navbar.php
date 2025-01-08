59840c75ed2e8d0d5e108febbf733820379bacb4
tiana-coding <102430225+tiana-coding@users.noreply.github.com>
2025-01-08 15:05:46 +0100

    modified:   mergedProject/include/navbar.php 	modified:   mergedProject/index.php
        alt tag beschreibungen hinzugefügt.

--- mergedProject/include/navbar.php
+++ mergedProject/include/navbar.php
@@ -4,7 +4,7 @@
  
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['DOCUMENT_ROOT'] . '/hotelprojekt-original/mergedProject/index.php'); ?>">
-      <img src="<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['DOCUMENT_ROOT'] . '/hotelprojekt-original/mergedProject/res/assets/img/pct_hotel-emoji.png'); ?>" height="28px"/></a>
+      <img src="<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['DOCUMENT_ROOT'] . '/hotelprojekt-original/mergedProject/res/assets/img/pct_hotel-emoji.png'); ?>" alt="hotel-emoji" height="28px"/></a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
